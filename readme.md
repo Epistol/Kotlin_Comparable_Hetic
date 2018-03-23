a4b286e696997589a22f884b6ba24459cf800acf
Epistol <mika63270@live.fr>
2018-03-24 00:16:12 +0100

    Essai 1 _ utiliser contains

---
+++ readme.md
@@ -0,0 +1,17 @@
+Essayez de répondre à la question, si vous n'y arrivez pas, essayez. Le minimum est de lister les étapes de l'algorithme en commentaire.
+• Le rendu doit être fait sous forme du fichier Kotlin (et uniquement le fichier extension .kt) dans un fichier ZIP avec votre prénom et votre nom (prenom-nom.zip)
+• Si vous utilisez le code d'une source externe, citez vos sources et expliquez pourquoi vous avez choisi ce code plutôt qu'un autre. Le code sera passé dans MOSS.
+Date de rendu: 23/03/2018
+
+Enoncé :
+*****************************************
+Le problème est le suivant.
+J'ai une liste avec pleins de valeurs numériques, non triées. Comment puis-je trouver si une valeur est présente dans la liste?
+class Arbre {
+var valeur : Int
+var gauche : Arbre?
+var droite : Arbre?
+fun find(i: Int) : Boolean
+fun inserer(i: Int)
+}
+*****************************************
--- src/AppliTest/main.kt
+++ src/AppliTest/main.kt
@@ -1,69 +1,50 @@
 package AppliTest
 
-open class Personne(p: String, n: String) {
 
-    constructor():this("Toto","Ce héros")
+// J'ai une liste avec pleins de valeurs numériques, non triées.
+// Sont-ils dans un tableau ?
 
-    var prenom = p
-    var nom = n
+class Arbre : Comparable<Arbre> {
 
-    open fun direBonjour(){
-        println("Bonjour")
+
+    var valeur: Int = 0
+    // 10 elements aléatoires dans un tableau
+    var table = IntArray(10)
+//
+//    var gauche: Arbre? = null
+//    var droite: Arbre? = null
+
+    fun init_tableau_valeurs() {
+        val n = table.size - 1
+        for (i in 1..n)
+            table[i] = (Math.random() * 100).toInt()
+    }
+
+    // Trouver si une valeur est présente dans la liste
+    // Return a boolean
+    fun find(i: Int): Boolean {
+        return table.contains(i)
+
+    }
+
+    override fun compareTo(other: Arbre): Int {
+        // Not used
+        return 0
+    }
+
+    // Rien à insérer
+    /* fun inserer(i: Int) {
+
+     }*/
+
+    fun main(args: Array<String>) {
+        init_tableau_valeurs()
+        if(find(42))
+            println("Trouvé")
+        else {
+            println("Pas trouvé :/ ")
+        }
+
     }
 
 }
-
-open class VIP(p:String, n:String) : Personne(p,n){
-    override fun direBonjour(){
-        println("Wesh")
-    }
-}
-
-
-open class Avion{
-    open fun faireLePLein(){}
-}
-
-interface Bateau{
-    open fun faireLePLein(){}
-}
-class Hydravion : Avion(), Bateau{
-    override fun faireLePLein() {
-//        On choisi de ne pas choisir quel truc remplir
-//        super.faireLePLein()
-//        On rempli l'avion puis le bateau
-        super<Avion>.faireLePLein()
-        super<Bateau>.faireLePLein()
-    }
-}
-
-fun faire(x:Int, y: Int, f:((Int,Int) -> Int)) : Int){
-    return f(x,y)
-}
-
-//BERK
-
-faire(1,2){
-    a,b -> a+b
-}
-
-// VERSION COURTE
-
-class Personne2(var p: String, var n: String)
-
-//Avec des valeurs pré-remplies
-
-class Personne3(var p: String = "Toto", var n: String = "Pas important")
-//{}
-
-
-
-fun main(args: Array<String>) {
-    var a = fibo(12)
-    println("Hello $a")
-}
-
-fun fibonacci(n: Int, a: Long, b: Long): Long {
-    return if (n > 0) fibonacci(n - 1, b, a + b) else a
-}
-
